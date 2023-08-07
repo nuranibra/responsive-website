9ddf36222c9408512e7ff84e2bc6778100188d23
Nuran28 <ibrahimovnuran008@gmail.com>
2023-08-07 14:05:55 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// var manStar = prompt("Saytimizi 1 den 10 qeder bir reqem ile deyerlendirin...")
+
+// var resultManStar = manStar;
+
+// if(manStar == 1 && 2 && 3){
+//     alert("Saytimiza duzelish etmeye komek et")
+// } else if (manStar == 4 || 5 || 6){
+//     alert("Senin fikrince saytimiz normaldi")
+// } else if (manStar == 7 || 8 || 9){
+//     alert("Ela")
+// } else if (manStar == 10){
+//     alert("beyeneceyini bilirdim")
+// }
